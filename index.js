9874c8813549c0faa7c32e5d30ea2b123fadaf23
Xica Felix <xicasfelix@gmail.com>
2021-11-12 18:55:19 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainID = document.getElementById('main');
+mainID.remove();
+
+const newHeader= document.createElement('h1');
+newHeader.id='victory';
+newHeader.textContent="Xica is the champion";
+document.body.append(newHeader);
